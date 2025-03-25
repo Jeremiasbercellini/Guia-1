8035fccca026aa21bbb3def486518339cb0fe7dc
Jeremiasbercellini <167231647+Jeremiasbercellini@users.noreply.github.com>
2025-03-25 12:11:53 -0300

    ej dos casi terminado

---
+++ Assets/scripts/DatosDelUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    [SerializeField] int edadUsuario;
+    [SerializeField] float altura;
+    [SerializeField] bool donante;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
